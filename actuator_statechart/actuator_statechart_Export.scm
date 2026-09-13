1f97499ea0c33c67634e4a5c2b784307a77f26d2
lifiuba <lifranco@fi.uba.ar>
2026-09-13 18:30:43 -0300

    Add files via upload
    Correciones sobre el agregado de estado de parpadeo

--- actuator_statechart/actuator_statechart_Export.scm
+++ actuator_statechart/actuator_statechart_Export.scm
@@ -14,10 +14,10 @@
         "id": "00ffb6d1-d225-4bc0-8b73-7df9987f57b7",
         "attrs": {
           "name": {
-            "text": "actuator_statechart Export"
+            "text": "actuator_statechart Copy Export"
           },
           "specification": {
-            "text": "@EventDriven\n@SuperSteps(no)\n\ninterface:\n    in event EV_LED_ON\n    in event EV_LED_OFF\n\ninternal:\n    var led_state: boolean"
+            "text": "@EventDriven\n@SuperSteps(no)\n\ninterface:\n    in event EV_LED_ON\n    in event EV_LED_OFF\n    in event EV_LED_BLINKING\n"
           }
         },
         "z": 1
@@ -28,8 +28,8 @@
           "y": -130
         },
         "size": {
-          "width": 131,
-          "height": 104
+          "height": 104,
+          "width": 131
         },
         "type": "State",
         "attrs": {
@@ -38,8 +38,8 @@
             "fontSize": 11
           }
         },
-        "id": "78895411-2d12-4656-ac9d-e7700d8766ef",
-        "z": 2
+        "z": 2,
+        "id": "78895411-2d12-4656-ac9d-e7700d8766ef"
       },
       {
         "position": {
@@ -53,8 +53,8 @@
         "type": "Entry",
         "entryKind": "Initial",
         "attrs": {},
-        "id": "eab2a46a-29e3-4cb4-9b5d-f71a36b5febc",
         "z": 7,
+        "id": "eab2a46a-29e3-4cb4-9b5d-f71a36b5febc",
         "embeds": [
           "fa00140b-c60b-41b9-ab7c-445a429942d8"
         ]
@@ -73,14 +73,14 @@
         "attrs": {
           "label": {
             "refX": "50%",
-            "textAnchor": "middle",
             "refY": "50%",
-            "textVerticalAnchor": "middle"
+            "textVerticalAnchor": "middle",
+            "textAnchor": "middle"
           }
         },
-        "id": "fa00140b-c60b-41b9-ab7c-445a429942d8",
+        "parent": "eab2a46a-29e3-4cb4-9b5d-f71a36b5febc",
         "z": 8,
-        "parent": "eab2a46a-29e3-4cb4-9b5d-f71a36b5febc"
+        "id": "fa00140b-c60b-41b9-ab7c-445a429942d8"
       },
       {
         "type": "Transition",
@@ -89,15 +89,15 @@
           "id": "eab2a46a-29e3-4cb4-9b5d-f71a36b5febc"
         },
         "target": {
-          "id": "78895411-2d12-4656-ac9d-e7700d8766ef",
           "anchor": {
-            "name": "topLeft",
             "args": {
+              "rotate": true,
               "dx": "58.333%",
-              "dy": "8.333%",
-              "rotate": true
-            }
+              "dy": "8.333%"
+            },
+            "name": "topLeft"
           },
+          "id": "78895411-2d12-4656-ac9d-e7700d8766ef",
           "priority": true
         },
         "connector": {
@@ -122,45 +122,45 @@
             "attrs": {}
           }
         ],
-        "id": "bdf45e53-0980-4d4e-92eb-baf53a5c2f59",
-        "z": 9,
         "router": {
           "name": "orthogonal"
         },
-        "vertices": []
+        "vertices": [],
+        "z": 9,
+        "id": "bdf45e53-0980-4d4e-92eb-baf53a5c2f59"
       },
       {
         "position": {
-          "x": 191,
-          "y": -138
+          "x": 187,
+          "y": -131
         },
         "size": {
-          "width": 134,
-          "height": 113
+          "height": 104,
+          "width": 131
         },
         "type": "State",
         "attrs": {
           "name": {
-            "text": "ST_ACT_ON",
+            "text": "ST_ACT_ACTUANDO",
             "fontSize": 11
           }
         },
-        "id": "b4528c02-7c8b-46e8-870b-09d5ad5d49a2",
-        "z": 10
+        "z": 14,
+        "id": "6b4e909e-c192-4c1a-a2ee-04b486f0e331"
       },
       {
         "type": "Transition",
         "attrs": {},
         "source": {
-          "id": "78895411-2d12-4656-ac9d-e7700d8766ef"
+          "id": "6b4e909e-c192-4c1a-a2ee-04b486f0e331"
         },
         "target": {
-          "id": "b4528c02-7c8b-46e8-870b-09d5ad5d49a2",
+          "id": "78895411-2d12-4656-ac9d-e7700d8766ef",
           "anchor": {
             "name": "topLeft",
             "args": {
-              "dx": "2.239%",
-              "dy": "32.743%",
+              "dx": "67.176%",
+              "dy": "43.269%",
               "rotate": true
             }
           },
@@ -173,7 +173,7 @@
           {
             "attrs": {
               "text": {
-                "text": "EV_LED_ON / led_state = true"
+                "text": "EV_LED_OFF"
               }
             },
             "position": {}
@@ -192,8 +192,83 @@
             "attrs": {}
           }
         ],
-        "id": "8d3ca10e-1e6d-446c-a1d0-3906ed666c1b",
-        "z": 11,
+        "id": "cb6ffecc-72d5-4daa-8755-4b91b054e747",
+        "z": 15,
+        "router": {
+          "name": "orthogonal"
+        },
+        "vertices": [
+          {
+            "x": 119,
+            "y": -85
+          }
+        ]
+      },
+      {
+        "position": {
+          "x": 558,
+          "y": -142
+        },
+        "size": {
+          "height": 113,
+          "width": 134
+        },
+        "type": "State",
+        "attrs": {
+          "name": {
+            "text": "ST_ACT_ON",
+            "fontSize": 11
+          }
+        },
+        "z": 19,
+        "id": "b4528c02-7c8b-46e8-870b-09d5ad5d49a2"
+      },
+      {
+        "type": "Transition",
+        "attrs": {},
+        "source": {
+          "id": "6b4e909e-c192-4c1a-a2ee-04b486f0e331"
+        },
+        "target": {
+          "id": "b4528c02-7c8b-46e8-870b-09d5ad5d49a2",
+          "anchor": {
+            "name": "topLeft",
+            "args": {
+              "dx": "24.627%",
+              "dy": "53.982%",
+              "rotate": true
+            }
+          },
+          "priority": true
+        },
+        "connector": {
+          "name": "rounded"
+        },
+        "labels": [
+          {
+            "attrs": {
+              "text": {
+                "text": "EV_LED_ON"
+              }
+            },
+            "position": {}
+          },
+          {
+            "attrs": {
+              "label": {
+                "text": "2"
+              }
+            }
+          },
+          {
+            "attrs": {}
+          },
+          {
+            "attrs": {}
+          }
+        ],
+        "id": "48f7205c-33c0-43fe-8371-efceead3e807",
+        "z": 20,
         "router": {
           "name": "orthogonal"
         },
@@ -206,12 +281,12 @@
           "id": "b4528c02-7c8b-46e8-870b-09d5ad5d49a2"
         },
         "target": {
-          "id": "78895411-2d12-4656-ac9d-e7700d8766ef",
+          "id": "6b4e909e-c192-4c1a-a2ee-04b486f0e331",
           "anchor": {
             "name": "topLeft",
             "args": {
-              "dx": "99.237%",
-              "dy": "59.615%",
+              "dx": "97.71%",
+              "dy": "64.423%",
               "rotate": true
             }
           },
@@ -224,7 +299,7 @@
           {
             "attrs": {
               "text": {
-                "text": "EV_LED_OFF / led_state = false"
+                "text": "EV_LED_BLINKING"
               }
             },
             "position": {}
@@ -243,12 +318,76 @@
             "attrs": {}
           }
         ],
-        "id": "a44f42f8-5223-43a7-a218-d259e44670ee",
-        "z": 12,
+        "id": "52809dc3-75bf-47d0-8958-4b76c7a706ad",
+        "z": 20,
         "router": {
           "name": "orthogonal"
         },
         "vertices": []
+      },
+      {
+        "type": "Transition",
+        "attrs": {},
+        "source": {
+          "id": "78895411-2d12-4656-ac9d-e7700d8766ef"
+        },
+        "target": {
+          "id": "6b4e909e-c192-4c1a-a2ee-04b486f0e331",
+          "anchor": {
+            "name": "topLeft",
+            "args": {
+              "dx": "0%",
+              "dy": "63.462%",
+              "rotate": true
+            }
+          },
+          "priority": true
+        },
+        "connector": {
+          "name": "rounded"
+        },
+        "labels": [
+          {
+            "attrs": {
+              "text": {
+                "text": "EV_LED_BLINKING"
+              }
+            },
+            "position": {}
+          },
+          {
+            "attrs": {
+              "label": {
+                "text": "1"
+              }
+            }
+          },
+          {
+            "attrs": {}
+          },
+          {
+            "attrs": {}
+          }
+        ],
+        "id": "5bfc75b6-539a-4d81-bc08-13ae2dd5157e",
+        "z": 21,
+        "router": {
+          "name": "orthogonal"
+        },
+        "vertices": [
+          {
+            "x": -1,
+            "y": -20
+          },
+          {
+            "x": 34,
+            "y": -20
+          },
+          {
+            "x": 157,
+            "y": -44
+          }
+        ]
       }
     ]
   },
@@ -282,8 +421,8 @@
           "getters": false
         },
         "IdentifierSettings": {
-          "moduleName": "ActuatorStatechart",
-          "statemachinePrefix": "actuatorStatechart",
+          "moduleName": "ActuatorStatechartCopy",
+          "statemachinePrefix": "actuatorStatechartCopy",
           "separator": "_",
           "headerFilenameExtension": "h",
           "sourceFilenameExtension": "c"
